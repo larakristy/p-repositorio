6d0ff1811a1d91c57875176b3fa59bd525e337ad
larakristy <72520656+larakristy@users.noreply.github.com>
2020-10-07 14:03:47 -0500

    Update main.js

--- data_includes/main.js
+++ data_includes/main.js
@@ -2,24 +2,3 @@
 
 //Inativa os prefixos do PennController (sem esse comando os códigos não funcionam)
 PennController.ResetPrefix(null);
-//Define a sequência de telas do experimento
-Sequence("Participante", "Instrucoes", randomize("Experimento"), SendResults(), "Final");
-//Cria um cabeçalho. Todos os comandos dentro do cabeçalho serão rodados automaticamente antes de cada "trial"
-Header(
-//Define que todo texto será impresso na tela e que o tamanho da fonte será "1.2em"
-         defaultText
-            .css("font-size","1.2em")
-            .print()
-         ,
-//Define que toda caixa de texto será impressa na tela e que o tamanho da fonte será "1.2em"
-         defaultTextInput
-            .css("font-size","1.2em")
-            .print()
-         ,
-//Define que todo botão será impresso na tela, que o tamanho da fonte será "1.2em" e que o participante será obrigado a interagir com ele para prosseguir com o experimento
-         defaultButton
-            .css("font-size","1.2em")
-            .center()
-            .print()
-            .wait()       
-)
