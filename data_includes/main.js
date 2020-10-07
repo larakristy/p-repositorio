8681b2ca99ebdaea6bcc2978a8bb9e12bc0835f8
larakristy <72520656+larakristy@users.noreply.github.com>
2020-10-07 14:06:44 -0500

    Update main.js

--- data_includes/main.js
+++ data_includes/main.js
@@ -2,3 +2,45 @@
 
 //Inativa os prefixos do PennController (sem esse comando os códigos não funcionam)
 PennController.ResetPrefix(null);
+//Define a sequência de telas do experimento
+Sequence("Participante", "Instrucoes", randomize("Experimento"), SendResults(), "Final");
+
+//Cria um cabeçalho. Todos os comandos dentro do cabeçalho serão rodados automaticamente antes de cada "trial"
+Header(
+//Define que todo texto será impresso na tela e que o tamanho da fonte será "1.2em"
+         defaultText
+            .css("font-size","1.2em")
+            .print()
+         ,
+//Define que toda caixa de texto será impressa na tela e que o tamanho da fonte será "1.2em"
+         defaultTextInput
+            .css("font-size","1.2em")
+            .print()
+         ,
+//Define que todo botão será impresso na tela, que o tamanho da fonte será "1.2em" e que o participante será obrigado a interagir com ele para prosseguir com o experimento
+         defaultButton
+            .css("font-size","1.2em")
+            .center()
+            .print()
+            .wait()       
+)//Cria uma nova tela - Tela de coleta de dados do participante//Cria uma caixa com seletores nomeada "Escolaridade" para que o participante selecione sua escolaridade
+         newDropDown("Escolaridade", "Selecione sua escolaridade")
+                  .add("Médio completo", "Superior em curso", "Superior completo", "Pós-graduação")
+                  .css("font-size","1.2em")
+                  .print()
+                  .log() //Envia para o arquivo "results" a opção selecionada pelo participante 
+         ,
+newTrial("Participante",
+
+//Cria o texto "Bem-Vindos!"
+         newText("<p>Bem-Vindos!</p>")
+         ,
+         newText("<p>Neste experimento, você vai ouvir uma frase e depois deve escolher a melhor opção de interpretação para ela.</p>")
+         ,
+         newText("<p>Por favor, escreva seu NOME COMPLETO na caixa abaixo.</p>")
+         ,
+//Cria uma caixa de texto nomedada "Nome" para receber o nome do participante  
+         newTextInput("Nome")
+         ,
+         newText("<p>Agora selecione sua ESCOLARIDADE na caixa abaixo e aperte o botão 'Iniciar' para começar </p>")
+         , 
